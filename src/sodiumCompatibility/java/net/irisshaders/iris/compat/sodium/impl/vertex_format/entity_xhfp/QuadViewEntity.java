894e88a695aa41184674d36bd6a6554cb948d3bc
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-03-30 13:26:36 +0800

    Update QuadViewEntity.java

--- src/sodiumCompatibility/java/net/irisshaders/iris/compat/sodium/impl/vertex_format/entity_xhfp/QuadViewEntity.java
+++ src/sodiumCompatibility/java/net/irisshaders/iris/compat/sodium/impl/vertex_format/entity_xhfp/QuadViewEntity.java
@@ -2,64 +2,75 @@ package net.irisshaders.iris.compat.sodium.impl.vertex_format.entity_xhfp;
 
 import net.irisshaders.iris.vertices.views.QuadView;
 import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.system.Pointer;
 
 import java.nio.ByteBuffer;
 
 public abstract class QuadViewEntity implements QuadView {
-	long writePointer;
-	int stride;
+  long writePointer;
+  int stride;
 
-	@Override
-	public float x(int index) {
-		return getFloat(writePointer - stride * (3L - index));
-	}
+  @Override
+  public float x(int index) {
+    return getFloat(index, 0);
+  }
 
-	@Override
-	public float y(int index) {
-		return getFloat(writePointer + 4 - stride * (3L - index));
-	}
+  @Override
+  public float y(int index) {
+    return getFloat(index, 4);
+  }
 
-	@Override
-	public float z(int index) {
-		return getFloat(writePointer + 8 - stride * (3L - index));
-	}
+  @Override
+  public float z(int index) {
+    return getFloat(index, 8);
+  }
 
-	@Override
-	public float u(int index) {
-		return getFloat(writePointer + 16 - stride * (3L - index));
-	}
+  @Override
+  public float u(int index) {
+    return getFloat(index, 16);
+  }
 
-	@Override
-	public float v(int index) {
-		return getFloat(writePointer + 20 - stride * (3L - index));
-	}
+  @Override
+  public float v(int index) {
+    return getFloat(index, 20);
+  }
 
-	abstract float getFloat(long writePointer);
+  abstract float getFloat(int index, int offset);
 
-	public static class QuadViewEntityUnsafe extends QuadViewEntity {
-		public void setup(long writePointer, int stride) {
-			this.writePointer = writePointer;
-			this.stride = stride;
-		}
+  public static class QuadViewEntityUnsafe extends QuadViewEntity {
+    private int stride;
+    private long writePointer;
 
-		@Override
-		float getFloat(long writePointer) {
-			return MemoryUtil.memGetFloat(writePointer);
-		}
-	}
+    public void setup(long writePointer, int stride) {
+      this.writePointer = writePointer & ~((1L << Pointer.POINTER_SHIFT) - 1);
+      this.stride = (stride + (1 << Pointer.POINTER_SHIFT) - 1) &
+          ~((1L << Pointer.POINTER_SHIFT) - 1);
+    }
 
-	public static class QuadViewEntityNio extends QuadViewEntity {
-		private ByteBuffer buffer;
+    @Override
+    float getFloat(int index, int offset) {
+      long address = writePointer + ((long) stride * index & 0xFFFFFFFFL) +
+          (offset & 0xFFFFFFFFL);
+      return Float.intBitsToFloat(MemoryUtil.memGetInt(address));
+    }
+  }
 
-		public void setup(ByteBuffer buffer, int writePointer, int stride) {
-			this.buffer = buffer;
-			this.writePointer = writePointer;
-			this.stride = stride;
-		}
+  public static class QuadViewEntityNio extends QuadViewEntity {
+    private ByteBuffer buffer;
+    private int stride;
+    private long writePointer;
 
-		@Override
-		float getFloat(long writePointer) {
-			return buffer.getFloat((int) writePointer);
-		}
-	}
+    public void setup(ByteBuffer buffer, int writePointer, int stride) {
+      this.buffer = buffer;
+      this.writePointer = writePointer;
+      this.stride = stride;
+    }
+
+    @Override
+    float getFloat(int index, int offset) {
+      long address = ((long) stride * index & 0xFFFFFFFFL) +
+          (offset & 0xFFFFFFFFL);
+      return buffer.getFloat((int) address);
+    }
+  }
 }
